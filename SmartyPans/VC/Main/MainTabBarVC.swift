2b8466b6082571ec468a9c62b9cca5b2d68ed3d5
rb512 <rahul@smartypans.io>
2018-09-10 08:51:13 -0700

    Added asset for pan discovery screen

--- SmartyPans/VC/Main/MainTabBarVC.swift
+++ SmartyPans/VC/Main/MainTabBarVC.swift
@@ -59,13 +59,13 @@ extension MainTabBarVC:UITabBarControllerDelegate{
         let index = tabBarController.viewControllers?.index(of: viewController)!
         //Add Recipe
         if index == 1{
-            //let vc = storyboard?.instantiateViewController(withIdentifier: "PanPairVC")
-            //navigationController?.show(vc, sender: nil)
-            let storyBoard: UIStoryboard = UIStoryboard(name: "Recipe", bundle: nil)
-            let vc = storyBoard.instantiateViewController(withIdentifier: "AddIngredientVC") as! AddIngredientViewController
-            //navigationController?.present(vc, animated: true, completion: nil)
+            let vc = storyboard?.instantiateViewController(withIdentifier: "PanPairVC")
+//            navigationController?.show(vc, sender: nil)
+//            let storyBoard: UIStoryboard = UIStoryboard(name: "Recipe", bundle: nil)
+//            let vc = storyBoard.instantiateViewController(withIdentifier: "AddIngredientVC") as! AddIngredientViewController
+            navigationController?.present(vc!, animated: true, completion: nil)
             
-            let navVC: UINavigationController = UINavigationController(rootViewController: vc)
+            let navVC: UINavigationController = UINavigationController(rootViewController: vc!)
             navVC.navigationBar.isHidden = true
             navigationController?.present(navVC, animated: true, completion: nil)
             
